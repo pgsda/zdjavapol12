2873b353269f41f35ec814581b1a24761d064ee8
Your Name <you@example.com>
2020-10-04 12:55:53 +0200

    Lista produktow

--- pobierz-produkty.js
+++ pobierz-produkty.js
@@ -2,7 +2,13 @@ function downloadProducts() {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onload = function() {
-        console.log(this.responseText);
+        var section = document.querySelector("main section");
+        section.innerHTML = "";
+        var productsList = JSON.parse(this.responseText);
+
+        for(var i = 0; i < productsList.length; i++) {
+            section.innerHTML += productsList[i].name;
+        }
     }
 
     xhttp.open("GET", "produkty.json", true);
